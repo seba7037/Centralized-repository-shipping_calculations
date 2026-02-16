1b89a223f5ef72088ebbe2540a5011a89cd0858a
seba7037 <sebgaard@gmail.com>
2026-02-16 10:57:00 +0100

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,3 +1,4 @@
+import numpy as np
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
@@ -6,8 +7,17 @@ rate = float(input("Enter the shipping rate per kilogram: "))
 
 ## Calculate shipping cost
 shipping_cost = weight * rate
+empty_list = []
+empty_list.append(shipping_cost)
+shipping_array = np.array(empty_list)
+sorte_array = np.argsort(shipping_array)
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
 # Here is a new update by seba7037
+
+# here is another update by Seba7027
+## calculating index
+
+print(f" index
